47ce08bc6623b15a9015dd99e1c69da08e8b4756
Just-a-random-username <arjunagnihotri1903@gmail.com>
2023-10-05 23:06:02 +0530

    Day 1 of Restarted Challenge

---
+++ Day 1.java
@@ -0,0 +1,28 @@
+class Day1 {
+    public String longestCommonPrefix(String[] strs)  {
+        int l = 0;
+        boolean condition = false;
+        while(true){
+            if (strs.length==1){
+                return strs[0];
+            }
+            for (int i = 0 ; i < strs.length;i++){
+                if (l<strs[i].length()){
+                    char c = strs[0].charAt(l);
+                    if (c!=strs[i].charAt(l)){
+                        condition = true;
+                        break;
+                    }
+                }else{
+                    condition = true;
+                    break;
+                }
+            }
+            if (condition){
+                break;
+            }else{
+                l++;
+            }
+        }return strs[0].substring(0,l);
+    }
+}
