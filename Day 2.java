fbba14f2c28563aba38adc65f6d717d842009840
Just-a-random-username <arjunagnihotri1903@gmail.com>
2023-10-09 22:37:18 +0530

    Couldn't solve for few days due to family events

---
+++ Day 2.java
@@ -0,0 +1,26 @@
+// LEETCODE : 28. Find the Index of the First Occurrence in a String
+
+class Day 2 {
+    class Solution {
+        public int strStr(String haystack, String needle){
+            int l1 = haystack.length();
+            int l2 = needle.length();
+            int index = -1;
+            if (l2>l1){
+                return -1;
+            }
+            for (int i = 0;i<l1;i++){
+                if (haystack.charAt(i)==needle.charAt(0)){
+                    if (l2+i>l1){
+                        return -1;
+                    }else{
+                        if (haystack.substring(i,l2+i).equals(needle)){
+                            index =i;
+                            break;
+                        }
+                    }
+                }
+            }return index;
+        }
+    }
+}
